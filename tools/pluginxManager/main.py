b1dcf56382128f39b726be5e629dc2a84f241266
Austin Diviness <austdi@microsoft.com>
2015-03-11 14:35:26 -0700

    tried adding documentation

--- tools/pluginxManager/main.py
+++ tools/pluginxManager/main.py
@@ -2,119 +2,133 @@ import sys, string, os
 from Tkinter import *
 from tkFileDialog import *
 import tkMessageBox
-import fnmatch
+import fnmatch # TODO why is this here?
 from PluginsUtil import *
 
-currentPath = os.path.join(os.path.realpath(__file__), '..')
-cashPath=os.path.join(currentPath,'.cash')
-cocosPath = os.path.join(currentPath,'../..')
+currentPath = os.path.join(os.path.realpath(__file__), '..') # TODO is this the current working directory or the path to this file? is there a distinction?
+cashPath = os.path.join(currentPath, '.cash')
+cocosPath = os.path.join(currentPath, '../..')
 pluginsPath = os.path.join(cocosPath, 'plugins')
-# Create Local Cash Folder
-if not os.path.exists(cashPath):
-    os.makedirs(cashPath)
+plugins = {}
+# list of platforms that are found in each plugin's directory
+pluginPlatforms = []
 
-
-
-plugins={}
-pluginPlatforms=[]
-def loadPlugins(rootdir='.'):
+def loadPlugins(rootdir = '.'):
+    '''walks the plugins directory, adding each plugin to the plugins dict and updating pluginPlatforms with any new platforms found."
+    '''
     for d in os.listdir(rootdir):
-        p=os.path.join(rootdir,d)
+        p = os.path.join(rootdir, d)
         if os.path.isdir(p):
-            plugins[d]={'platforms':[], 'path':p}
+            plugins[d] = {'platforms': [], 'path': p}
             for platform in os.listdir(p):
                 if (platform.startswith('proj.')):
                     if platform not in pluginPlatforms:
                         pluginPlatforms.append(platform)
                     importPlugin(pluginsPath, d, platform)
                     plugins[d]['platforms'].append(platform)
-    return plugins
-
-importPluginX('proj.win8.1-universal')
-
-loadPlugins(pluginsPath)
+    return plugins # TODO why return plugins if its a global?
 
 
-
-master = Tk()
-
-master.geometry('500x500');
-
-master.title('pluginx Manager')
-
-def createImportPluginsFrame(parent, dir):
-    group = LabelFrame(parent, text="Plugins")
-
-    group.grid(row=1, padx=10, pady=10)
+def createImportPluginsFrame(parent, d): 
+    '''creates portion of gui that lists found plugins and platforms.
+    @param parent: tkinter object to attach more tkinter objects to.
+    @param d: directory path shown on the plugin table
+    '''
+    # TODO where is this used?
     def getProject():
-        dir=askdirectory(parent=parent)
-        Label(group, text=dir).pack()
+        '''queries for directory string, creates label based off of it'''
+        d = askdirectory(parent = parent) # TODO d should be returned somehow, so that it can be updated
+        print "here asking"
+        Label(group, text = d).pack() # TODO group should be a function parameter
 
-    pluginTable = LabelFrame(parent, text=dir)
-    pluginTable.grid(row=1,padx=10,pady=10)
-    i=0
-    checks={}
+    def processChanges():
+        '''callback function that finds any changes made to the gui's check marks and TODO
+        '''
+        msg = ""
+        changes = []
+        for check in checks: 
+            if(checks[check]['value'] != checks[check]['var'].get()):
+                change = {'plugin': checks[check]['plugin'], 'platform': checks[check]['platform']}
+                if(checks[check]['value'] == 0):
+                    msg += "Add    "
+                    change['api'] = 'add'
+                else:
+                    msg += "Remove "
+                    change['api'] = 'rm'
+                changes.append(change)
+                msg += checks[check]['plugin'] + " " + checks[check]['platform'] + '\n' # TODO string formatting would look cleaner
+        if(msg == ""):
+            msg = "Warning: No changes on input"
+        # TODO should display a different dialog if no changes were found, having yes and no buttons on an error dialog is confusing
+        result = tkMessageBox.askquestion("Confirm Plugin Changes", msg, icon = 'warning')
+        if result == 'yes':
+            for change in changes:
+                if change['api'] == 'rm':
+                    rmPlugin(d, change['plugin'], change['platform'])
+                else:
+                    addPlugin(d, change['plugin'], change['platform'])
+
+    group = LabelFrame(parent, text = "Plugins")
+    group.grid(row = 1, padx = 10, pady = 10)
+    pluginTable = LabelFrame(parent, text = d)
+    pluginTable.grid(row = 1, padx = 10, pady = 10)
+    i = 0 # TODO i, j tracking should be replaced with enumerate function
+    checks = {} # dict of CheckButton -> dict
     for p in pluginPlatforms:
-        Label(pluginTable, text=p[5:]).grid(row=0,column=i+1)
-        loadedPlugins=getPlugins(dir, p)
-        j=0
+        Label(pluginTable, text = p[5:]).grid(row = 0, column = i + 1) # strips first 5 chars to remove "proj."
+        loadedPlugins = getPlugins(d, p)
+        j = 0
         for key in plugins:
-            Label(pluginTable,text=key).grid(row=j+1,column=0)
-            checkValue=IntVar()
-            check = Checkbutton(pluginTable,variable=checkValue)
-            checks[check]={'platform':p,'plugin':key, 'value':0, 'var':checkValue}
+            Label(pluginTable, text = key).grid(row = j + 1, column = 0)
+            checkValue = IntVar()
+            check = Checkbutton(pluginTable, variable = checkValue)
+            checks[check] = {'platform': p, 'plugin': key, 'value': 0, 'var': checkValue}
             if (p in plugins[key]['platforms']):
-                check.grid(row=j+1,column=i+1)
+                check.grid(row = j + 1, column = i + 1)
                 if(key in loadedPlugins):
                     check.select()
-                    checks[check]['value']=1
-            j+=1
-        i+=1
-    def processChanges():
-        msg=""
-        changes=[]
-        for check in checks:
-            if(checks[check]['value'] != checks[check]['var'].get()):
-                change={'plugin':checks[check]['plugin'],'platform':checks[check]['platform']}
-                if(checks[check]['value']==0):
-                    msg+="Add    "
-                    change['api']='add'
-                else:
-                    msg+="Remove "
-                    change['api']='rm'
-                changes.append(change)
-                msg+=checks[check]['plugin']+" "+checks[check]['platform']+'\n'
-        if(msg==""):
-            msg="Warning: No changes on input"
-        result=tkMessageBox.askquestion("Confirm Plugin Changes", msg, icon='warning')
-        if result=='yes':
-            for change in changes:
-                if change['api']=='rm':
-                    rmPlugin(dir, change['plugin'],change['platform'])
-                else:
-                    addPlugin(dir, change['plugin'],change['platform'])
+                    checks[check]['value'] = 1
+            j += 1
+        i += 1
+    Button(parent, text = "Apply Changes", command = processChanges).grid(row = 1, column = 1, padx = 10, pady = 10)
 
-    Button(parent,text="Apply Changes",command=processChanges).grid(row=1,column=1, padx=10,pady=10)
 def isCocosProject(path):
+    '''returns true if directory pointed to by path has a 'Classes' subdirectory'''
     for d in os.listdir(path):
-        if(d=="Classes"):
-            return 1
+        if(d == "Classes"):
+            return 1 # TODO should return True or False, not ints
     return 0
 
 def createOpenProjectFrame(parent):
-    group = LabelFrame(parent, text="Root Project")
-    group.grid(row=0,  padx=10, pady=10)
+    group = LabelFrame(parent, text = "Root Project")
+    group.grid(row = 0,  padx = 10, pady = 10)
     def getProject():
-        dir=askdirectory(parent=parent)
-        if isCocosProject(dir):
-            createImportPluginsFrame(master, dir)
+        d = askdirectory(parent = parent)
+        if isCocosProject(d):
+            createImportPluginsFrame(master, d) # TODO master should be passed into function
         else:
             tkMessageBox.showwarning(
                 "Invalid Cocos Project",
-                "Cannot detect cocos project in this location\n(%s)" % dir
+                "Cannot detect cocos project in this location\n(%s)" % d
             )
-    Button(group, text="Open ...", command=getProject).pack()
+    Button(group, text = "Open ...", command = getProject).pack()
 
+
+#def main():
+# Create Local Cash Folder
+if not os.path.exists(cashPath):
+    os.makedirs(cashPath)
+importPluginX('proj.win8.1-universal')
+loadPlugins(pluginsPath)
+master = Tk()
+master.geometry('500x500');
+master.title('pluginx Manager')
 createOpenProjectFrame(master)
-createImportPluginsFrame(master, 'E:/git/work/cocos2d-x/plugin/samples/HelloPlugins')
+createImportPluginsFrame(master, 'T:/Cocos2dx/plugin/samples/HelloPlugins') # TODO hardcoded path
 master.mainloop()
+
+#if __name__ == "__main__":
+#    main()
+        
+
+    
--- tools/pluginxManager/win8_1_universal.py
+++ tools/pluginxManager/win8_1_universal.py
@@ -1,70 +1,78 @@
 import os
 import subprocess
 from xml.dom.minidom import parse, parseString
+
 currentPath = os.path.join(os.path.realpath(__file__), '..')
-cashPath=os.path.join(currentPath,'.cash')
+cashPath = os.path.join(currentPath, '.cash') # TODO duplicates in main.py
+
 def importUniversalSolution(solutionPath, plugin):
-    return
+    return # TODO why is this return here?
     #check if project is C++ plugin or C# plugin project
     if not os.path.isfile(solutionPath):
         print "Could not locate", solutionPath
         return False
-    platforms=['Win32']
-    result=0
+    platforms = ['Win32']
+    result = 0
     for platform in platforms:
-        destPath =os.path.join(cashPath,plugin,'proj.win8.1-universal',platform)+'\\'
-        result|=subprocess.call(
+        destPath = os.path.join(cashPath, plugin, 'proj.win8.1-universal', platform) + '\\' # TODO should replace '\\' with os.path.sep
+        result |= subprocess.call(
         ['msbuild',
-        solutionPath,
-        '/p:Configuration=Debug',
-        '/p:Platform='+platform,'/t:Rebuild',
-        '/p:OutDir='+destPath])
+         solutionPath,
+         '/p:Configuration=Debug',
+         '/p:Platform=' + platform,
+         '/t:Rebuild',
+         '/p:OutDir=' + destPath])
     if result != 0:
         return False
     return True
 
 def getPlugins(path):
+    '''returns a list of strings that are names for each pluginx plugin found in the in the *.Windows and *.WindowsPhone vcxproj files for the cocos2d-x project
+    @param path: path to the root of a cocos2d-x project
+    '''
     plugins = []
-    projpath=path+'\\proj.win8.1-universal'
+    projpath = path + '\\proj.win8.1-universal' # TODO should use os.path.join
     for d in os.listdir(projpath):
-        if d.endswith('Windows') or d.endswith('WindowsPhone'):
-
-            if d.endswith('Windows'):
-                platform="Windows"
+        if d.endswith('Windows') or d.endswith('WindowsPhone'): # TODO should be using os.path.splitext instead of endswith
+            if d.endswith('Windows'): # TODO why is this if statement here? is there a better way then double if statements?
+                platform = "Windows"
             else:
-                platform="WindowsPhone"
+                platform = "WindowsPhone"
 
             projectFile = os.path.join(projpath, d)
-            for g in os.listdir(projectFile):
-                if g.endswith('.vcxproj'):
-                    projectFile = os.path.join(projectFile, g)
+            for g in os.listdir(projectFile): # TODO change for loop & if check to filter to avoid break
+                if g.endswith('.vcxproj'): # TODO should use os.path.splitext instead of endswith
+                    projectFile = os.path.join(projectFile, g) # TODO no guarantee that projectFile is actually set (there might not be a vcxproj file)
                     break
                 
             dom = parse(projectFile)
-            for project in dom.childNodes:
-                PluginManagerItems = None
+            for project in dom.childNodes: # TODO change to filter if possible, to avoid break
+                PluginManagerItems = None # TODO what is this? why is it here?
                 for child in project.getElementsByTagName("ItemGroup"):
                     if child.hasAttribute('Label'):
                         if child.attributes["Label"].value.startswith("pluginx:"):
-                            plugins.append(child.attributes["Label"].value[8:])
+                            plugins.append(child.attributes["Label"].value[8:]) # stripping first 8 chars removes "pluginx:" from front of string
                             break
     print plugins
     return plugins
 
 def addPlugin(path, plugin):
-    projpath=path+'\\proj.win8.1-universal'
-    print "Adding Win8.1-Universal Plugin",plugin, "to", path
+    '''rewrites vcxproj file to add xml elements to include plugins 
+    @param path: TODO
+    @param plugin:
+    '''
+    projpath = path + '\\proj.win8.1-universal' # TODO should use os.path.join
+    print "Adding Win8.1-Universal Plugin", plugin, "to", path
     for d in os.listdir(projpath):
-        if d.endswith('Windows') or d.endswith('WindowsPhone'):
-
+        if d.endswith('Windows') or d.endswith('WindowsPhone'): # TODO should use os.path.splitext
             if d.endswith('Windows'):
-                platform="Windows"
+                platform = "Windows"
             else:
-                platform="WindowsPhone"
+                platform = "WindowsPhone"
 
             projectFile = os.path.join(projpath, d)
-            for g in os.listdir(projectFile):
-                if g.endswith('.vcxproj'):
+            for g in os.listdir(projectFile): # TODO should use filter to avoid break
+                if g.endswith('.vcxproj'): # TODO should use os.path.splitext
                     projectFile = os.path.join(projectFile, g)
                     break
                 
@@ -76,33 +84,34 @@ def addPlugin(path, plugin):
                         if "PluginManagerItems" == child.attributes["Label"].value:
                             PluginManagerItems = child
                             break
-                if PluginManagerItems==None:
-                    PluginManagerItems=dom.createElement("ItemGroup")
-                    PluginManagerItems.attributes["Label"]="PluginManagerItems"
+                if PluginManagerItems == None:
+                    PluginManagerItems = dom.createElement("ItemGroup")
+                    PluginManagerItems.attributes["Label"] = "PluginManagerItems"
                     project.appendChild(PluginManagerItems)
 
-                deploymentChild=dom.createElement("DeploymentContent")
+                deploymentChild = dom.createElement("DeploymentContent")
                 deploymentChild.appendChild(dom.createTextNode('True'))
-                PluginChild=dom.createElement('None')
-                PluginChild.attributes['Label']=plugin
+                PluginChild = dom.createElement('None')
+                PluginChild.attributes['Label'] = plugin
                 PluginChild.appendChild(deploymentChild)
-                # todo add all DLLS
-                DllPath=os.path.join(cashPath,plugin,'proj.win8.1-universal','$(Platform)', plugin+'.'+platform,plugin+'.'+platform+'.dll')
-                PluginChild.attributes["Include"]=DllPath
+                # TODO add all DLLS
+                DllPath = os.path.join(cashPath, plugin, 'proj.win8.1-universal', '$(Platform)', plugin + '.' + platform, plugin + '.' + platform + '.dll')
+                PluginChild.attributes["Include"] = DllPath
                 PluginManagerItems.appendChild(PluginChild)
-                file_handle = open(projectFile,"wb")
+                file_handle = open(projectFile, "wb")
                 dom.writexml(file_handle)
     return 0
 
+# TODO this doesn't do anything
 def rmPlugin(path, plugin):
     return 0
 
 def importPlugin(pluginPath, plugin):
     print "Importing Windows 8.1 Universal Plugin:", plugin
-    solutionPath = os.path.join(pluginPath, plugin, "proj.win8.1-universal", plugin+'.sln')
+    solutionPath = os.path.join(pluginPath, plugin, "proj.win8.1-universal", plugin + '.sln')
     return importUniversalSolution(solutionPath, plugin)
 
 def importPluginX():
     print "Importing Windows 8.1 Universal Plugin Library:"
     solutionPath = os.path.join(cashPath, "../../../protocols/proj.win8.1-universal/pluginx.sln")
-    return importUniversalSolution(solutionPath,'pluginx')
+    return importUniversalSolution(solutionPath, 'pluginx')
