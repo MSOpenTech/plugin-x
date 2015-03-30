56fbc71ba92d53c8ce81480a68f07ec2059a526a
Austin Diviness <austdi@microsoft.com>
2015-03-30 09:16:46 -0700

    removed old comments

--- plugins/microsoftiap/proj.win8.1-universal/microsoftiap/microsoftiap.Shared/plugin.cpp
+++ plugins/microsoftiap/proj.win8.1-universal/microsoftiap/microsoftiap.Shared/plugin.cpp
@@ -1,6 +1,8 @@
 ﻿#include "pch.h"
 #include "plugin.h"
 
+#include <iostream>
+
 using namespace concurrency;
 using namespace Windows::Foundation::Collections;
 using namespace Platform;
@@ -17,13 +19,12 @@ namespace microsoftiap {
 	private:
 		bool debugMode;
         Windows::UI::Core::CoreDispatcher^ dispatcher;
-	public:
 
+	public:
         virtual event OnPayResultHandler^ OnPayResult;
 
 		microsoftiap() {
 			debugMode = false;
-            //dispatcher =  Windows::ApplicationModel::Core::CoreApplication::MainView->Dispatcher;
             dispatcher = nullptr;
 		}
 
@@ -45,16 +46,40 @@ namespace microsoftiap {
 			debugMode = bDebug;
 		}
 
+        // TODO
+        virtual void callFuncWithParam(Platform::String^ funcName, Windows::Foundation::Collections::IVector<IPluginParam^>^ params) {
+            return;
+        }
+
+        // TODO
+        virtual Platform::String^ callStringFuncWithParam(Platform::String^ funcName, Windows::Foundation::Collections::IVector<IPluginParam^>^ params) {
+            return L"";
+        }
+
+        // TODO
+        virtual int callIntFuncWithParam(Platform::String^ funcName, Windows::Foundation::Collections::IVector<IPluginParam^>^ params) {
+            return 0;
+        }
+
+        // TODO
+        virtual bool callBoolFuncWithParam(Platform::String^ funcName, Windows::Foundation::Collections::IVector<IPluginParam^>^ params) {
+            return false;
+        }
+
+        // TODO
+        virtual float callFloatFuncWithParam(Platform::String^ funcName, Windows::Foundation::Collections::IVector<IPluginParam^>^ params) {
+            return 0;
+        }
+
 		virtual void configDeveloperInfo(IMap<Platform::String^, Platform::String^>^ devInfo) {
 			log("configuring developer info");
-			// TODO this should really be moved out of this function and called directly by the cocos app, but first
-			// IProtocol needs to support the PluginProtocol's arbitrary function-calling functions.
 			// The path looked up by "windows_store_proxy" must use \ to split directories and not /
 #ifdef TESTING
 			loadWindowsStoreProxyFile(devInfo->Lookup("windows_store_proxy"));
 #endif
 		}
 
+        // checks that the product can in fact be purchased before attempting to buy
 		virtual void payForProduct(IMap<Platform::String^, Platform::String^>^ info) {
 			Platform::String^ product = nullptr;
 			bool foundProduct = false;
@@ -82,38 +107,20 @@ namespace microsoftiap {
 			}
 		}
 
+        // send payment result to cocos app
 		void sendPayResult(PayResultCodeEnum ret, Platform::String^ msg) {
             log(msg);
             log("calling listener");
-            Platform::Collections::Map<Platform::String^, Platform::String^>^ info; // TODO what should this be?
-            // TODO send information to listener
             OnPayResult(ret, msg);
 		}
 
-		// non interface functions
         void loadWindowsStoreProxyFile(Platform::String^ filePath) {
-            /*
-            auto uri = ref new Windows::Foundation::Uri(Platform::String::Concat("ms-appx:///", filePath));
-            create_task(StorageFile::GetFileFromApplicationUriAsync(uri)).then([this](task<StorageFile^> currentTask) {
-            StorageFile^ file;
-            try {
-            file = currentTask.get();
-            create_task(CurrentAppSimulator::ReloadSimulatorAsync(file)).then([this] {
-            return;
-            }).wait();
-            }
-            catch (Platform::InvalidArgumentException^ e) {
-            OutputDebugString(e->Message->Data());
-            }
-            }).wait();
-            */
             StorageFolder^ installationFolder = Windows::ApplicationModel::Package::Current->InstalledLocation;
             create_task(installationFolder->GetFileAsync(filePath)).then([this](task<StorageFile^> currentTask) {
                 StorageFile^ file;
                 try {
                     file = currentTask.get();
                     create_task(CurrentAppSimulator::ReloadSimulatorAsync(file)).then([this] {
-                    //this->setInitialized(true);
                     }).wait();
                 }
                 catch (Platform::Exception^ e) {
@@ -122,39 +129,11 @@ namespace microsoftiap {
             }).wait();
         }
 
-        // TODO
-        virtual void callFuncWithParam(Platform::String^ funcName, Windows::Foundation::Collections::IVector<IPluginParam^>^ params) {
-            return;
-        }
-
-        // TODO
-        virtual Platform::String^ callStringFuncWithParam(Platform::String^ funcName, Windows::Foundation::Collections::IVector<IPluginParam^>^ params) {
-            return L"";
-        }
-
-        // TODO
-        virtual int callIntFuncWithParam(Platform::String^ funcName, Windows::Foundation::Collections::IVector<IPluginParam^>^ params) {
-            return 0;
-        }
-
-        // TODO
-        virtual bool callBoolFuncWithParam(Platform::String^ funcName, Windows::Foundation::Collections::IVector<IPluginParam^>^ params) {
-            return false;
-        }
-
-        // TODO
-        virtual float callFloatFuncWithParam(Platform::String^ funcName, Windows::Foundation::Collections::IVector<IPluginParam^>^ params) {
-            return 0;
-        }
-
-        virtual void setDispatcher(Windows::UI::Core::CoreDispatcher^ dispatcher) {
-            this->dispatcher = dispatcher;
-        }
-
+        // waits on async action
         void sendResult(Windows::Foundation::IAsyncOperation <Platform::String^> ^ action, Platform::String^ product) {
             try {
                 create_task(action).then([this, product](Platform::String^ msg) {
-                    LicenseInformation^ licenseInfo = CurrentAppSimulator::LicenseInformation;
+                    LicenseInformation^ licenseInfo = this->getLicenseInformation();
                     if (licenseInfo->ProductLicenses->Lookup(product)->IsActive) {
                         sendPayResult(PayResultCodeEnum::kPaySuccess, msg); // TODO what should the msg be?
                     }
@@ -169,19 +148,32 @@ namespace microsoftiap {
             }
         }
 
+		LicenseInformation^ getLicenseInformation() {
+#ifdef TESTING
+			return CurrentAppSimulator::LicenseInformation;
+#else
+			return CurrentApp::LicenseInformation;
+#endif
+		}
+
+        virtual void setDispatcher(Windows::UI::Core::CoreDispatcher^ dispatcher) {
+            this->dispatcher = dispatcher;
+        }
+
 	private:
 		void log(Platform::String^ msg) {
 			if (debugMode) {
-				wprintf(L"%ls\n", msg->Data()); // TODO should do real logging
+                std::wcout << msg->Data() << std::endl; // TODO should do real logging, and this doesn't seem to actually be output to console anyway
 			}
 		}
 
+        // requests product purchase on correct thread
         void purchaseProduct(Platform::String^ product) {
             dispatcher->RunAsync(
                 Windows::UI::Core::CoreDispatcherPriority::Normal,
                 ref new Windows::UI::Core::DispatchedHandler([this, product]() {
                 try {
-                    auto ret = CurrentAppSimulator::RequestProductPurchaseAsync(product, false);
+                    auto ret = CurrentAppSimulator::RequestProductPurchaseAsync(product, true);
                     this->sendResult(ret, product);
                 }
                 catch (Exception^ e) {
@@ -191,14 +183,6 @@ namespace microsoftiap {
             }));
 
         }
-
-		LicenseInformation^ getLicenseInformation() {
-#ifdef TESTING
-			return CurrentAppSimulator::LicenseInformation;
-#else
-			return CurrentApp::LicenseInformation;
-#endif
-		}
 
 	};
 } // end namespace
