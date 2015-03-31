ed8228ecb99094a7e6950035e3d0fd2436880780
Austin Diviness <austdi@microsoft.com>
2015-03-31 09:37:35 -0700

    work on getting proper PluginProtocol implementation

--- protocols/platform/winrt/PluginProtocol.cpp
+++ protocols/platform/winrt/PluginProtocol.cpp
@@ -1,17 +1,21 @@
 
+#include <cstdarg>
+#include <collection.h>
 #include "PluginProtocol.h"
 #include "util.h"
-#include <cstdarg>
 #include "winrtInterface_h.h"
 #include "PluginMap.h"
+#include "PluginParamWinRT.h"
 using namespace cocos2d::plugin;
 using namespace pluginx;
 using namespace Microsoft::WRL;
 using namespace Microsoft::WRL::Wrappers;
 using namespace ABI::winrtInterface;
 using namespace pluginx::util;
+using namespace Platform::Collections;
+using namespace pluginparam;
 PluginProtocol::~PluginProtocol() {
-	// TODO
+    // TODO
 }
 
 std::string PluginProtocol::getPluginVersion() {
@@ -27,135 +31,133 @@ void PluginProtocol::setDebugMode(bool bDebug) {
 }
 
 void PluginProtocol::callFuncWithParam(const char* funcName, PluginParam* param, ...) {
-	std::vector<PluginParam*> allParams;
-	if (param != NULL) {
-		allParams.push_back(param);
-		PluginParam* pArg = NULL;
-		va_list argp;
-		va_start(argp, param);
-		while (true) {
-			pArg = va_arg(argp, PluginParam*);
-			if (pArg == NULL) {
-				break;
-			}
-			allParams.push_back(pArg);
-		}
-		va_end(argp);
-	}
-	callFuncWithParam(funcName, allParams);
+    std::vector<PluginParam*> allParams;
+    if (param != NULL) {
+        allParams.push_back(param);
+        PluginParam* pArg = NULL;
+        va_list argp;
+        va_start(argp, param);
+        while (true) {
+            pArg = va_arg(argp, PluginParam*);
+            if (pArg == NULL) {
+                break;
+            }
+            allParams.push_back(pArg);
+        }
+        va_end(argp);
+    }
+    callFuncWithParam(funcName, allParams);
 }
 
 void PluginProtocol::callFuncWithParam(const char* funcName, std::vector<PluginParam*> params) {
-	//Platform::String^ platFuncName = util::charArrayToPlatformString(funcName);
-	//Vector<PluginParamWinRT^>^ platParams = util::stdVectorToPlatformVector(params);
-	//ProtocolHelper::GlobalCallback->callFuncWithParam(util::charArrayToPlatformString(getPluginName()), platFuncName, platParams);
+    Platform::String^ platFuncName = util::charArrayToPlatformString(funcName);
+    Vector<PluginParamWinRT^>^ platParams = util::stdVectorToPlatformVector(params);
+    //ProtocolHelper::GlobalCallback->callFuncWithParam(util::charArrayToPlatformString(getPluginName()), platFuncName, platParams);
+    //PluginMap::mapIProtocol[this]->callFuncWithParam(platFuncName, platParams); // TODO
 }
 
 std::string PluginProtocol::callStringFuncWithParam(const char* funcName, PluginParam* param, ...) {
-	std::vector<PluginParam*> allParams;
-	if (param != NULL) {
-		allParams.push_back(param);
-		PluginParam* pArg = NULL;
-		va_list argp;
-		va_start(argp, param);
-		while (true) {
-			pArg = va_arg(argp, PluginParam*);
-			if (pArg == NULL) {
-				break;
-			}
-			allParams.push_back(pArg);
-		}
-		va_end(argp);
-	}
-	return callStringFuncWithParam(funcName, allParams);
+    std::vector<PluginParam*> allParams;
+    if (param != NULL) {
+        allParams.push_back(param);
+        PluginParam* pArg = NULL;
+        va_list argp;
+        va_start(argp, param);
+        while (true) {
+            pArg = va_arg(argp, PluginParam*);
+            if (pArg == NULL) {
+                break;
+            }
+            allParams.push_back(pArg);
+        }
+        va_end(argp);
+    }
+    return callStringFuncWithParam(funcName, allParams);
 }
 
 std::string PluginProtocol::callStringFuncWithParam(const char* funcName, std::vector<PluginParam*> params) {
-  return "";
-	//Platform::String^ platFuncName = util::charArrayToPlatformString(funcName);
-	//Vector<PluginParamWinRT^>^ platParams = util::stdVectorToPlatformVector(params);
-	//return util::platformStringToCharArray(ProtocolHelper::GlobalCallback->callStringFuncWithParam(util::charArrayToPlatformString(getPluginName()), platFuncName, platParams));
+    Platform::String^ platFuncName = util::charArrayToPlatformString(funcName);
+    Vector<PluginParamWinRT^>^ platParams = util::stdVectorToPlatformVector(params);
+    //return util::platformStringToCharArray(ProtocolHelper::GlobalCallback->callStringFuncWithParam(util::charArrayToPlatformString(getPluginName()), platFuncName, platParams));
+    //return util::PlatformStringToStdString(PluginMap::mapIProtocol[this]->callStringFuncWithParam(platFuncName, platParams)); // TODO
+    return "";
 }
 
 int PluginProtocol::callIntFuncWithParam(const char* funcName, PluginParam* param, ...) {
-	std::vector<PluginParam*> allParams;
-	if (param != NULL) {
-		allParams.push_back(param);
-		PluginParam* pArg = NULL;
-		va_list argp;
-		va_start(argp, param);
-		while (true) {
-			pArg = va_arg(argp, PluginParam*);
-			if (pArg == NULL) {
-				break;
-			}
-			allParams.push_back(pArg);
-		}
-		va_end(argp);
-	}
-	return callIntFuncWithParam(funcName, allParams);
+    std::vector<PluginParam*> allParams;
+    if (param != NULL) {
+        allParams.push_back(param);
+        PluginParam* pArg = NULL;
+        va_list argp;
+        va_start(argp, param);
+        while (true) {
+            pArg = va_arg(argp, PluginParam*);
+            if (pArg == NULL) {
+                break;
+            }
+            allParams.push_back(pArg);
+        }
+        va_end(argp);
+    }
+    return callIntFuncWithParam(funcName, allParams);
 }
 
 int PluginProtocol::callIntFuncWithParam(const char* funcName, std::vector<PluginParam*> params) {
-  /*	Platform::String^ platFuncName = util::charArrayToPlatformString(funcName);
-    Vector<PluginParamWinRT^>^ platParams = util::stdVectorToPlatformVector(params);
-    return ProtocolHelper::GlobalCallback->callIntFuncWithParam(util::charArrayToPlatformString(getPluginName()), platFuncName, platParams);
-    */ return 0;
+    //Platform::String^ platFuncName = util::charArrayToPlatformString(funcName);
+    //Vector<PluginParamWinRT^>^ platParams = util::stdVectorToPlatformVector(params);
+    //return ProtocolHelper::GlobalCallback->callIntFuncWithParam(util::charArrayToPlatformString(getPluginName()), platFuncName, platParams);
+    return 0;
 }
 
 bool PluginProtocol::callBoolFuncWithParam(const char* funcName, PluginParam* param, ...) {
-	std::vector<PluginParam*> allParams;
-	if (param != NULL) {
-		allParams.push_back(param);
-		PluginParam* pArg = NULL;
-		va_list argp;
-		va_start(argp, param);
-		while (true) {
-			pArg = va_arg(argp, PluginParam*);
-			if (pArg == NULL) {
-				break;
-			}
-			allParams.push_back(pArg);
-		}
-		va_end(argp);
-	}
-	return callBoolFuncWithParam(funcName, allParams);
+    std::vector<PluginParam*> allParams;
+    if (param != NULL) {
+        allParams.push_back(param);
+        PluginParam* pArg = NULL;
+        va_list argp;
+        va_start(argp, param);
+        while (true) {
+            pArg = va_arg(argp, PluginParam*);
+            if (pArg == NULL) {
+                break;
+            }
+            allParams.push_back(pArg);
+        }
+        va_end(argp);
+    }
+    return callBoolFuncWithParam(funcName, allParams);
 }
 
 bool PluginProtocol::callBoolFuncWithParam(const char* funcName, std::vector<PluginParam*> params) {
-  /*
-	Platform::String^ platFuncName = util::charArrayToPlatformString(funcName);
-	Vector<PluginParamWinRT^>^ platParams = util::stdVectorToPlatformVector(params);
-	return ProtocolHelper::GlobalCallback->callBoolFuncWithParam(util::charArrayToPlatformString(getPluginName()), platFuncName, platParams);
-  */
-  return 0;
+    //Platform::String^ platFuncName = util::charArrayToPlatformString(funcName);
+    //Vector<PluginParamWinRT^>^ platParams = util::stdVectorToPlatformVector(params);
+    //return ProtocolHelper::GlobalCallback->callBoolFuncWithParam(util::charArrayToPlatformString(getPluginName()), platFuncName, platParams);
+    return 0;
 }
 
 float PluginProtocol::callFloatFuncWithParam(const char* funcName, PluginParam* param, ...) {
-	std::vector<PluginParam*> allParams;
-	if (param != NULL) {
-		allParams.push_back(param);
-		PluginParam* pArg = NULL;
-		va_list argp;
-		va_start(argp, param);
-		while (true) {
-			pArg = va_arg(argp, PluginParam*);
-			if (pArg == NULL) {
-				break;
-			}
-			allParams.push_back(pArg);
-		}
-		va_end(argp);
-	}
-	return callFloatFuncWithParam(funcName, allParams);
+    std::vector<PluginParam*> allParams;
+    if (param != NULL) {
+        allParams.push_back(param);
+        PluginParam* pArg = NULL;
+        va_list argp;
+        va_start(argp, param);
+        while (true) {
+            pArg = va_arg(argp, PluginParam*);
+            if (pArg == NULL) {
+                break;
+            }
+            allParams.push_back(pArg);
+        }
+        va_end(argp);
+    }
+    return callFloatFuncWithParam(funcName, allParams);
 }
 
 float PluginProtocol::callFloatFuncWithParam(const char* funcName, std::vector<PluginParam*> params) {
-  return 0;
-  /*
-	Platform::String^ platFuncName = util::charArrayToPlatformString(funcName);
-	Vector<PluginParamWinRT^>^ platParams = util::stdVectorToPlatformVector(params);
-	return ProtocolHelper::GlobalCallback->callFloatFuncWithParam(util::charArrayToPlatformString(getPluginName()), platFuncName, platParams);
-  */
+    //Platform::String^ platFuncName = util::charArrayToPlatformString(funcName);
+    //Vector<PluginParamWinRT^>^ platParams = util::stdVectorToPlatformVector(params);
+    //return ProtocolHelper::GlobalCallback->callFloatFuncWithParam(util::charArrayToPlatformString(getPluginName()), platFuncName, platParams);
+    return 0;
 }
 
