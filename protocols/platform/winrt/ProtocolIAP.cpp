4dfeb0d51c77c3aeb6fdd3c6903508f5186c9fd0
Austin Diviness <austdi@microsoft.com>
2015-03-31 09:29:35 -0700

    updated ProtocolIAP implementation

--- protocols/platform/winrt/ProtocolIAP.cpp
+++ protocols/platform/winrt/ProtocolIAP.cpp
@@ -2,31 +2,44 @@
 #include "ProtocolIAP.h"
 //#include "ProtocolHelper.h"
 #include "util.h"
+#include "winrtInterface_h.h"
+#include "PluginMap.h"
 
 using namespace cocos2d::plugin;
 using namespace pluginx;
+using namespace winrtInterface;
 
 ProtocolIAP::ProtocolIAP() {
-  _listener = nullptr;
+    _listener = nullptr;
 }
 
 ProtocolIAP::~ProtocolIAP() {
-	// TODO
+    // TODO
 }
 
 void ProtocolIAP::configDeveloperInfo(TIAPDeveloperInfo devInfo) {
-	//TIAPDeveloperInfoWinRT devInfoWinRT = util::stdStrMapToPlatformStrMap(&devInfo);
-	//ProtocolHelper::GlobalCallback->configIAPDeveloperInfo(util::charArrayToPlatformString(const_cast<char*>(getPluginName())), devInfoWinRT);
+    PluginMap::mapIProtocolIAP[this]->configDeveloperInfo(util::stdStrMapToPlatformStrMap(&devInfo));
 }
 
 void ProtocolIAP::payForProduct(TProductInfo info) {
-	// TODO
+    PluginMap::mapIProtocolIAP[this]->payForProduct(util::stdStrMapToPlatformStrMap(&info));
 }
 
 void ProtocolIAP::setResultListener(PayResultListener* pListener) {
-  _listener = pListener;
+    _listener = pListener;
+    PluginMap::mapIProtocolIAP[this]->OnPayResult += ref new OnPayResultHandler([this](PayResultCodeEnum ret, Platform::String^ msg) {
+        this->onPayResult((PayResultCode)ret, util::PlatformStringToStdString(msg).c_str());
+    });
 }
 
-void onPayResult(PayResultCode ret, const char* msg) {
-	// TODO
+void ProtocolIAP::onPayResult(PayResultCode ret, const char* msg) {
+    // TODO
+    if (_listener != nullptr) {
+        std::map<std::string, std::string> empty;
+        _listener->onPayResult(ret, msg, empty); // TODO where does the product info come from?
+    }
+    if (_callback != nullptr) {
+        std::string message(msg);
+        _callback(ret, message);
+    }
 }
