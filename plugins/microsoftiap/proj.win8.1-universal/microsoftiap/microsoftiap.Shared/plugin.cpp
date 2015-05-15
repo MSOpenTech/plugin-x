a99a565c24befc92d0b072ba1e94734bc12d6fde
Dale Stammen <dalestam@microsoft.com>
2015-05-14 19:22:29 -0700

    fixed unsigned int type mismatch

--- plugins/microsoftiap/proj.win8.1-universal/microsoftiap/microsoftiap.Shared/plugin.cpp
+++ plugins/microsoftiap/proj.win8.1-universal/microsoftiap/microsoftiap.Shared/plugin.cpp
@@ -293,7 +293,7 @@ namespace microsoftiap {
             IVectorView<UnfulfilledConsumable^>^ consumables = unfulfilledConsumables();
             UnfulfilledConsumable^ c;
             Platform::String^ xmlString = L"<?xml version=\"1.0\" encoding=\"utf-8\"?><unfulfilled_consumables>";
-            for (int i = 0; i < consumables->Size; ++i) {
+            for (unsigned int i = 0; i < consumables->Size; ++i) {
                 c = consumables->GetAt(i);
                 xmlString += L"<consumable product_id=\"" + c->ProductId + L"\" transaction_id=\"" + c->TransactionId + L"\" />";
             }
@@ -303,7 +303,7 @@ namespace microsoftiap {
         Platform::String^ getEntitledDurables() {
             std::set<Platform::String^> consumableIds;
             IVectorView<UnfulfilledConsumable^>^ consumables = unfulfilledConsumables();
-            for (int i = 0; i < consumables->Size; ++i) {
+            for (unsigned int i = 0; i < consumables->Size; ++i) {
                 consumableIds.insert(consumables->GetAt(i)->ProductId);
             }
             typedef Windows::Foundation::Collections::IKeyValuePair<Platform::String^, ProductLicense^>  T_item;
